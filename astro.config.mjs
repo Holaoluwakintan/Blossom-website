99f6cb9d587def5d4874f05e8195787085823b26
Olaoluwa Michael <michaelolaoluwagab@gmail.com>
2026-08-31 16:30:38 +0100

    Reconcile download ecosystem and repair book assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
-
-export default defineConfig({
-  output: 'hybrid',
-  adapter: netlify(),
-  integrations: [tailwind()],
-});
-
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  vite: {
+    css: { postcss: {} },
+  },
 });
--- astro.vercel.config.mjs
+++ astro.vercel.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()],
+  vite: {
+    css: { postcss: {} },
+  },
 });
 
